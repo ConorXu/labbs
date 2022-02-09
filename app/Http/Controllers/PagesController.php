57bea5b01d8015bd2801fae79bf6777f2d3aaf8f
ConorXu <863540345@qq.com>
2022-02-09 16:03:20 +0800

    管理后台权限

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,8 +7,13 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     //
-    public function root()
+    public function permissionDenied()
     {
-        return view('pages.root');
+        // 如果当前用户有权限访问后台，直接跳转访问
+        if (config('administrator.permission')()) {
+            return redirect(url(config('administrator.uri')), 302);
+        }
+        // 否则使用视图
+        return view('pages.permission_denied');
     }
 }
